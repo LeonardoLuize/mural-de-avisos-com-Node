8d7e7b667eca43ffd833945165cae191703191d4
Leonardo <leonardo.luize2@gmail.com>
2020-12-29 15:53:32 -0300

    organized files

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express');
-const app = express();
 const PORT = 3000;
-const posts = require('./model/posts')
+const express = require('express');
+const apiRoute = require('./routes/api');
+const path = require('path');
+const app = express();
+
+
+app.use('/api', apiRoute);
+app.use('/', express.static(path.join(__dirname, "public")))
+
 
 app.listen(PORT, () => {
     console.log("Server runing on port: " + PORT);
@@ -9,17 +15,5 @@ app.listen(PORT, () => {
 
 
 
-app.get("/all", (req, res) => {
-    res.json(JSON.stringify(posts.getAll()));
-})
 
-app.get("/new", express.JSON, (req, res) => {
-    
-    let title = req.body.title;
-    let description = req.body.description;
-
-    posts.newPost(title, description);
-
-    res.send("Post adicionado");
-})
 
